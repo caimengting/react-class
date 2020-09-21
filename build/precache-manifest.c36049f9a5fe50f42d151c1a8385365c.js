af7f3ee9bc42cffa7c02569aba98a7c222fd5cb4
caimengting <3486069307@qq.com>
2020-09-21 08:46:40 +0800

    build

---
+++ build/precache-manifest.c36049f9a5fe50f42d151c1a8385365c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "700747e3197b65ef8309a6e3be3fd4a6",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5a8ca20baa222ec90ba9",
+    "url": "/static/js/2.3f3f865f.chunk.js"
+  },
+  {
+    "revision": "f4aac9c965aa45c554c7c1d1e3f69ce3",
+    "url": "/static/js/2.3f3f865f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9e4de0829c2059492b8e",
+    "url": "/static/js/main.b547c61f.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
