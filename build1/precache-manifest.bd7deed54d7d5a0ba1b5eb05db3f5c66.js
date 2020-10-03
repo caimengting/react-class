210bde7183ad42bede6ac159980195b5ac1c9468
caimengting <3486069307@qq.com>
2020-10-03 21:20:33 +0800

    add cmt xueli

---
+++ build1/precache-manifest.bd7deed54d7d5a0ba1b5eb05db3f5c66.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c70fb8eeaba6804d752f91fcf20afcf",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a36c6418e1323713a94d",
+    "url": "/static/js/2.665ca6e4.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.665ca6e4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f034ccd9fe8dc5ad114",
+    "url": "/static/js/main.7ab1d4eb.chunk.js"
+  },
+  {
+    "revision": "afd555cac37e17f8e0ee",
+    "url": "/static/js/runtime-main.d358ff35.js"
+  }
+]);
