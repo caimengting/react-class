a2e6595290395a4680965867ceedc0cdee8481c2
caimengting <3486069307@qq.com>
2020-10-25 09:42:49 +0800

    add xueli3

---
+++ build3/precache-manifest.7506e87d2bebabd1d9d2c41d0d96e6a1.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "93360679e86d31775fc60fe5d5a5f365",
+    "url": "/index.html"
+  },
+  {
+    "revision": "37767b43ecd6ef3970e9",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "d4f469d539992a41170d",
+    "url": "/static/css/main.5e6a83c5.chunk.css"
+  },
+  {
+    "revision": "37767b43ecd6ef3970e9",
+    "url": "/static/js/2.8f4128c6.chunk.js"
+  },
+  {
+    "revision": "5039e6fb7ed7bc0c7b07b989bcfe237c",
+    "url": "/static/js/2.8f4128c6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d4f469d539992a41170d",
+    "url": "/static/js/main.0c8830d7.chunk.js"
+  },
+  {
+    "revision": "fd4fbe75748714a085c9",
+    "url": "/static/js/runtime-main.50b9e298.js"
+  },
+  {
+    "revision": "ef20bc9e754c97741d53b81af7ec5a80",
+    "url": "/static/media/jiubei.ef20bc9e.png"
+  }
+]);
