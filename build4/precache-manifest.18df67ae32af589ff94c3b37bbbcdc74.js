421b6aab70a4c53029ed310db6f9c02a1daf6093
caimengting <3486069307@qq.com>
2020-11-10 14:33:12 +0800

    add taobao-thunk

---
+++ build4/precache-manifest.18df67ae32af589ff94c3b37bbbcdc74.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5be7c58182352ad2a10516c5b1fb6d3f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3fd03820e408a096cbec",
+    "url": "/static/css/2.f98a1de1.chunk.css"
+  },
+  {
+    "revision": "e4ba4ead08793ce79ec9",
+    "url": "/static/css/main.1e4a0b7a.chunk.css"
+  },
+  {
+    "revision": "3fd03820e408a096cbec",
+    "url": "/static/js/2.0b191862.chunk.js"
+  },
+  {
+    "revision": "5039e6fb7ed7bc0c7b07b989bcfe237c",
+    "url": "/static/js/2.0b191862.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e4ba4ead08793ce79ec9",
+    "url": "/static/js/main.fb590991.chunk.js"
+  },
+  {
+    "revision": "fd4fbe75748714a085c9",
+    "url": "/static/js/runtime-main.50b9e298.js"
+  }
+]);
